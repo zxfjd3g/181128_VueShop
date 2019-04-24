11ffb0d1fd384a724017858509a92e46fcf7575e
zxfjd3g <258147149@qq.com>
2019-04-24 10:40:23 +0800

    login

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,25 +2,42 @@ import MSite from '../pages/MSite/MSite.vue'
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 /*
 所有路由的数组
  */
 export default [
   {
     path: '/msite',
-    component: MSite
+    component: MSite,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/search',
-    component: Search
+    component: Search,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/order',
-    component: Order
+    component: Order,
+    meta: {
+      isShowFooter: true
+    }
   },
   {
     path: '/profile',
-    component: Profile
+    component: Profile,
+    meta: {
+      isShowFooter: true
+    }
+  },
+  {
+    path: '/login',
+    component: Login
   },
 
   { // 自动跳转的路由
